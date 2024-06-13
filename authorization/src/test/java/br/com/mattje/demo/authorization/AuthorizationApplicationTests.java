b3da9f5cf2542e3449950236f045fce68e434396
Alexandre Mattje <amattje@gmail.com>
2024-06-12 22:46:31 -0300

    initial

---
+++ authorization/src/test/java/br/com/mattje/demo/authorization/AuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mattje.demo.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
